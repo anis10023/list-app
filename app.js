90a5dbdad20b036529b537305571f97c8eea1e78
anis10023 <m.anis.rahman@outlook.com>
2023-09-22 23:07:31 +0100

    Improved styling

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 //&& Data structures -> will be using factory functions
 
-function Note(ID = null, information = null) {
+function Note(ID = 0, information = null) {
   const get_id = () => ID;
   const get_information = () => information;
+  const isClicked = false;
 
-  return { get_id, get_information };
+  return { ID, information, isClicked, get_id, get_information };
 }
 
 function Library() {
-  const notes = [];
+  let notes = [
+    {
+      ID: 1,
+      information: "Hello",
+    },
+  ];
 
   const add_note = (note) => notes.push(note);
 
@@ -33,39 +39,73 @@ const addNoteBtn = document.querySelector(".add-note-btn");
 const notesContainer = document.querySelector(".notes-container");
 
 //^^ Capturing user input
-
 function capture_user_input() {
+  const information = noteInput.value;
+  const ID = library.notes.length + 1;
 
-  const information = noteInput.value; 
-  const ID = library.length;
+  const newNote = Note(ID, information);
 
-  const newNote = Note(information, ID);
+  reset_inputs();
+
   return newNote;
 }
 
-//^^ Create new note on DOM
-function createNoteCard(){
-    const noteCard = document.createElement("div");
-    const information = document.createElement("p");
-    const ID = document.createElement("p");
-    const removeBtn = document.createElement("button");
+function update_notes_container() {
+  const lastNoteInLibrary = library.notes[library.notes.length - 1];
 
-    noteCard.classList.add(".note-card");
-    information.classList.add(".noteInformation");
-    ID.classList.add(".noteID");
-    removeBtn.classList.add(".remove-note-btn");
-
-
+  createNoteCard(lastNoteInLibrary.information, lastNoteInLibrary.ID);
 }
 
-//^ Render
-function render_note(e) {
-    e.preventDefault()
-    const newNote = capture_user_input();
+function reset_inputs() {
+  noteInput.value = "";
 }
+
+//^^ Create new note
+function createNoteCard(information, ID) {
+  const noteCard = document.createElement("div");
+  const information_p = document.createElement("p");
+  const ID_p = document.createElement("p");
+  const removeBtn = document.createElement("button");
+
+  noteCard.classList.add("note-card");
+  information_p.classList.add("noteInformation");
+  ID_p.classList.add("noteID");
+  removeBtn.classList.add("remove-note-btn");
+
+  information_p.textContent = `${information}`;
+  ID_p.textContent = `# ${ID}`;
+  removeBtn.textContent = "X";
+
+  notesContainer.appendChild(noteCard);
+  noteCard.appendChild(information_p);
+  noteCard.appendChild(ID_p);
+  noteCard.appendChild(removeBtn);
+
+  return removeBtn;
+}
+
+//^ Render onto DOM
+const render_note = () => {
+  const newNote = capture_user_input();
+
+  //! Does not render if input is empty
+  if (!newNote.information) {
+    return;
+  }
+
+  library.add_note(newNote);
+
+  update_notes_container(newNote);
+};
+
+//^ Remove from DOM
+const remove_card = () => {
+  library.remove_note();
+};
 
 //Event listeners
-addNoteBtn.onClick = ;
+addNoteBtn.addEventListener("click", (e) => render_note());
+removeBtn.addEventListener("click", (e) => remove_card());
 
-// Convert Values
-
+//! Displays precoded object in array
+update_notes_container();
